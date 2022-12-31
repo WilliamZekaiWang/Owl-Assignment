7c114e476b953d1f2b940cabf3d935dd0fa7a9a0
williamzekaiwang <williamzekaiwang@gmail.com>
2022-12-30 22:24:04 -0500

    prints out the assignment's course

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dict_to_dataframe import *
 import datetime
 from utils import *
 
-def main():
+def owl_assignments():
     # Get current day details to be able to compare to owl assignments
     current_day = str(datetime.now())[:19]
     print(f"The current date and time is: {current_day}")
@@ -20,13 +20,13 @@ def main():
             if df['Due Date'][index] != "":
                 days = int(days_away(current_day[0], df['Due Date'][index][0]).split(" ")[0])
                 if days == 0:
-                    todo['Today'].update({assignment: (hours_away(current_day[1], df['Due Date'][index][1])) + "Hours"})
+                    todo['Today'].update({assignment: ((hours_away(current_day[1], df['Due Date'][index][1])) + "Hour(s)", df['Course'][index])})
                 elif 0 <= days <= 7:
-                    todo['Within 7 Days'].update({assignment: str(days) + " Days"})
+                    todo['Within 7 Days'].update({assignment: (str(days) + " Day(s)", df['Course'][index])})
                 elif days > 7:
-                    todo['Greater Than 7 Days'].update({assignment: str(days) + " Days"})
+                    todo['Greater Than 7 Days'].update({assignment: (str(days) + " Day(s)", df['Course'][index])})
                 else:
-                    break
+                    break  # they should be sorted by date already so no need to check later dates
 
     display_assignments(todo)
     exit()
@@ -40,9 +40,11 @@ def display_assignments(dic):
     print("Upcoming Assignments")
     for key, value in dic.items():
         if value:
+            print("-"*100)
             print(f"{key}:")
-            for assignment, time in value.items():
-                print(f"   {assignment}: due in {time}")
+            for assignment, info in value.items():
+                print(f"   {info[1]}: {assignment}, due in {info[0]}")
+            print("-" * 100)
 
 if __name__ == "__main__":
-    main()
+    owl_assignments()
